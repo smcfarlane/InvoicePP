6b7e7b374a007ba77eae87e8f1d0a8e50c041ae2
stevenmcfarlane <stevemac72@gmail.com>
2015-05-08 11:15:34 -0700

    initial setup basic devise setup pages controller for static pages created
    Gems added
    devise
    bootstrap
    slim
    typescript
    
    Turbolinks disabled

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Get #features" do
+    it "returns http success" do
+      get :features
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
